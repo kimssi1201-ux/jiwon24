91f31dda4a879909758921c7aacb6b2056ebbbcc
kimssi1201-ux <kimssi1201@gmail.com>
2026-06-13 21:20:54 +0900

    Add hero app cafe image

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gov-support25-site-v9";
+const CACHE_NAME = "gov-support25-site-v11";
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,6 +11,7 @@ const ASSETS = [
   "./desktop.css",
   "./manifest.webmanifest",
   "./assets/claim-desk-hero.svg",
+  "./assets/hero-app-cafe.jpg?v=2",
   "./assets/illustration-support.svg",
   "./assets/illustration-refund.svg",
   "./assets/illustration-loan.svg",

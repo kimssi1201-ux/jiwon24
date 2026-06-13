51a115e069be4d4f3ee8b096dfdbe1a07f5f2b8b
kimssi1201-ux <kimssi1201@gmail.com>
2026-06-13 22:32:21 +0900

    Hide bottom navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gov-support25-site-v12";
+const CACHE_NAME = "gov-support25-site-v13";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,7 +7,7 @@ const ASSETS = [
   "./about.html",
   "./terms.html",
   "./privacy.html",
-  "./styles.css?v=3",
+  "./styles.css?v=4",
   "./desktop.css",
   "./manifest.webmanifest",
   "./assets/claim-desk-hero.svg",

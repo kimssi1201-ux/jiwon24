b6b080e23eb27b3d531a0996fa4f894b467d69c2
kimssi1201-ux <kimssi1201@gmail.com>
2026-06-13 15:29:49 +0900

    Add service worker cache

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "gov-support25-site-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./category.html",
+  "./policy.html",
+  "./about.html",
+  "./terms.html",
+  "./privacy.html",
+  "./styles.css",
+  "./site-data.js",
+  "./site.js",
+  "./manifest.webmanifest",
+  "./assets/claim-desk-hero.svg",
+  "./assets/icon-government-support25.svg",
+  "./assets/logo-government-support25.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => undefined));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
+    ),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});

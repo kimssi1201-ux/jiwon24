7e6b915787ca12a7d1e105d081258dc3f2724b48
kimssi1201-ux <kimssi1201@gmail.com>
2026-06-15 16:33:59 +0900

    Refresh cache for category match fix version 6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gov-support25-site-v69";
+const CACHE_NAME = "gov-support25-site-v70";
 const ASSETS = [
   "./",
   "./index.html",
@@ -22,7 +22,7 @@ const ASSETS = [
   "./age-merge-fix.js?v=1",
   "./target-foreigner-fix.js?v=2",
   "./region-label-fix.js?v=4",
-  "./category-match-fix.js?v=5",
+  "./category-match-fix.js?v=6",
   "./manifest.webmanifest",
   "./assets/claim-desk-hero.svg",
   "./assets/hero-phone-main.jpg?v=1",

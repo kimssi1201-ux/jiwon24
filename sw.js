dc31fcb6ad6c05e79d31c8dc24419ce5aa1545c7
kimssi1201-ux <kimssi1201@gmail.com>
2026-06-18 16:46:18 +0900

    Cache Gyeonggi Gwangju fix v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gov-support25-site-v84";
+const CACHE_NAME = "gov-support25-site-v85";
 const ASSETS = [
   "./",
   "./index.html",
@@ -23,7 +23,7 @@ const ASSETS = [
   "./age-merge-fix.js?v=1",
   "./target-foreigner-fix.js?v=2",
   "./region-label-fix.js?v=6",
-  "./gyeonggi-gwangju-search-fix.js?v=2",
+  "./gyeonggi-gwangju-search-fix.js?v=3",
   "./category-match-fix.js?v=11",
   "./manifest.webmanifest",
   "./assets/claim-desk-hero.svg",
